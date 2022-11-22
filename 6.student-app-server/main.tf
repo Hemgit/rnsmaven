954e01adbeb76413b01a772fe3c635e4dc0f3d30
dev1 <dev1@gmail.com>
2022-11-22 19:32:59 +0530

    Student app tf script updated

--- 6.student-app-server/main.tf
+++ 6.student-app-server/main.tf
@@ -13,46 +13,12 @@ provider "aws" {
   region = var.region
 }
 
-
-# Create a VPC
-
-resource "aws_vpc" "MyLab-VPC" {
-  cidr_block = var.cidr_block[0]
-
-  tags = {
-    Name = "My-Tomcat-VPC"
-  }
-
-}
-
-# Create Subnet
-
-resource "aws_subnet" "MyLab-Subnet1" {
-  vpc_id            = aws_vpc.MyLab-VPC.id
-  cidr_block        = var.cidr_block[1]
-  availability_zone = var.availability_zone
-  tags = {
-    Name = "My-Tomcat-Subnet1"
-  }
-}
-
-# Create Internet Gateway
-
-resource "aws_internet_gateway" "MyLab-IntGW" {
-  vpc_id = aws_vpc.MyLab-VPC.id
-
-  tags = {
-    Name = "My-Tomcat-InternetGW"
-  }
-}
-
-
 # Create Secutity Group
 
 resource "aws_security_group" "MyLab_Sec_Group" {
   name        = "Student App Security Group"
   description = "To allow inbound and outbound traffic to mylab"
-  vpc_id      = aws_vpc.MyLab-VPC.id
+  #vpc_id      = aws_vpc.MyLab-VPC.id
 
   dynamic "ingress" {
     iterator = port
@@ -79,28 +45,6 @@ resource "aws_security_group" "MyLab_Sec_Group" {
 
 }
 
-# Create route table and association
-
-resource "aws_route_table" "MyLab_RouteTable" {
-  vpc_id = aws_vpc.MyLab-VPC.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.MyLab-IntGW.id
-  }
-
-  tags = {
-    Name = "MyTomcat_Routetable"
-  }
-}
-
-# Create route table association
-resource "aws_route_table_association" "MyLab_Assn" {
-  subnet_id      = aws_subnet.MyLab-Subnet1.id
-  route_table_id = aws_route_table.MyLab_RouteTable.id
-}
-
-
 data "aws_ami" "aws_linux_2_latest" {
   most_recent = true
   owners      = ["amazon"]
@@ -117,12 +61,11 @@ resource "aws_instance" "Tomcat_Server" {
   instance_type               = var.instance_type
   key_name                    = var.keypair_name
   vpc_security_group_ids      = [aws_security_group.MyLab_Sec_Group.id]
-  subnet_id                   = aws_subnet.MyLab-Subnet1.id
   associate_public_ip_address = true
   user_data                   = file("./files/scripts/InstallTools.sh")
 
   tags = {
-    Name = "Tomcat-Server"
+    Name = "Student-App-Server"
   }
 
   provisioner "local-exec" {
